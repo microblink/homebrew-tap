1659420a4383f72c1a6516138a5e4d30554ad07f
Katarina Matic <katarina.matic@microblink.com>
2022-12-08 10:56:06 +0100

    add kirax auth

---
+++ Formula/kirax-auth.rb
@@ -0,0 +1,47 @@
+require "formula"
+
+class kirax-auth < Formula
+  desc "CLI for getting kirax tokens"
+  homepage "https://bitbucket.org/microblink/mlp-go-auth"
+  on_linux do
+    on_arm do
+        url "https://build-artifacts.mle.microblink.com/kirax-auth/v0.0.28/kirax-auth-linux-arm64.gz"
+    end
+    on_intel do
+        url "https://build-artifacts.mle.microblink.com/kirax-auth/v0.0.28/kirax-auth-linux-amd64.gz"
+    end
+  end
+
+  on_macos do
+    on_arm do
+        url "https://build-artifacts.mle.microblink.com/kirax-auth/v0.0.28/kirax-auth-darwin-arm64.gz"
+    end
+    on_intel do
+        url "https://build-artifacts.mle.microblink.com/kirax-auth/v0.0.28/kirax-auth-darwin-amd64.gz"
+    end
+  end
+
+  def install
+    if OS.linux?
+        if Hardware::CPU.arm?
+            system "mv kirax-auth-linux-arm64 kirax-auth"
+        else
+            system "mv kirax-auth-linux-amd64 kirax-auth"
+        end
+    end
+    if OS.mac?
+        if Hardware::CPU.arm?
+            system "mv kirax-auth-darwin-arm64 kirax-auth"
+        else
+            system "mv kirax-auth-darwin-amd64 kirax-auth"
+        end
+    end
+    bin.install "kirax-auth"
+  end
+
+  # Homebrew requires tests.
+  test do
+    assert_match "0.0.28", shell_output("#{bin}/kirax-auth -v", 2)
+  end
+
+end
